8e78c726b5f333b824f1d07fda8102004a272ca1
Althafnavas4 <nalthaf13@gmail.com>
2024-10-19 09:12:26 +0530

    add

---
+++ exam19.py
@@ -0,0 +1,22 @@
+# x=4
+# for i in range(1,4):
+#     a = 1
+#     for j in range(1,x):
+#         print(a,end=' ')
+#         a+=1
+#     x-=1
+#     print(' ')
+    
+# ----------------------
+
+ 
+# x=4
+# for i in range(1, 4):
+#     a = 65
+#     for j in range(1,x):
+
+#          print(chr(a), end=' ')
+#          a +=  1
+#     x -= 1
+#     print(' ')
+
